f738559e5bbfbd6128d23fef3bd25234bddd8e61
val rigodon <rvrigodon@gmail.com>
2016-12-27 16:34:32 -0500

    This commit is for the first part of video 13

---
+++ catch-of-the-day/src/components/addfishform.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class AddFishForm extends React.Component {
+render (){
+    return (
+
+
+       <form className="fish-edit">
+      <input type="text" placeholder="" />
+
+       <form>
+
+
+      )
+
+
+
+
+}
+
+
+
+
+}
+
+export default AddFishForm;
