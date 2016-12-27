8d69de6ef996c123a365f772e6fa64f2f7d98ea3
val rigodon <rvrigodon@gmail.com>
2016-12-27 00:21:32 -0500

    this commit is for lesson 6

---
+++ catch-of-the-day/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Header extends React.Component {
+render (){
+    return (
+
+
+        <p>Header </p>
+
+
+      )
+
+
+
+
+}
+
+
+
+
+}
+
+export default Header;
---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Order extends React.Component {
+render (){
+    return (
+
+
+        <p>Order </p>
+
+
+      )
+
+
+
+
+}
+
+
+
+
+}
+
+export default Order;
