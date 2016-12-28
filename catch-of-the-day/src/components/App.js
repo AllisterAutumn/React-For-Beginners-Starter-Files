4cf5988de8275fc4588342d54354dc377a8c055d
val rigodon <rvrigodon@gmail.com>
2016-12-28 13:17:35 -0500

    This commit is for lesson 20 - Updating the webpage in realtime

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
     this.addFish = this.addFish.bind(this);
      this.loadSamples = this.loadSamples.bind(this);
       this.addToOrder = this.addToOrder.bind(this);
+      this.updateFish = this.updateFish.bind(this);
 
 
     //get initial state
@@ -67,6 +68,13 @@ localStorage.setItem(`order-${this.props.params.storeId}`,
               this.setState({fishes })
   }
 
+  updateFish(key, updatedFish){
+    const fishes = {...this.state.fishes};
+    fishes[key] = updatedFish;
+    this.setState({fishes})
+
+
+  }
 
 loadSamples(){
 
@@ -104,7 +112,7 @@ return (
                 </ul>
             </div>
           <Order fishes={this.state.fishes} order={this.state.order}  params={this.props.params} />
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}  fishes={this.state.fishes} updateFish={this.updateFish}/>
         </div>
 
 
