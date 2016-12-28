52159224ad3c4c532d9b02fbf53bcf7d8e299a9f
val rigodon <rvrigodon@gmail.com>
2016-12-28 12:17:26 -0500

    This commit is for firebase and local storage

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -23,22 +23,38 @@ class App extends React.Component {
   }
 
 componentWillMount(){
+  //this runs right before the app is rendered
 this.ref = base.syncState( `${this.props.params.storeId}/fishes`,
   {
 
 context: this,
 state: 'fishes'
-
-
   });
 
-}
+
+//check if there is any order in local storage
+const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+
+if(localStorageRef){
+//update our app component's order state
+this.setState({
+  order: JSON.parse(localStorageRef)
+        })
+      }
+    }
 
 componentWillUnmount(){
 base.removeBinding(this.ref);
 
 
 }
+
+componentWillUpdate(nextProps, nextState){
+localStorage.setItem(`order-${this.props.params.storeId}`,
+  JSON.stringify(nextState.order))
+
+}
+
 
 
   addFish(fish){
@@ -87,7 +103,7 @@ return (
                   }
                 </ul>
             </div>
-          <Order fishes={this.state.fishes} order={this.state.order}  />
+          <Order fishes={this.state.fishes} order={this.state.order}  params={this.props.params} />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
         </div>
 
