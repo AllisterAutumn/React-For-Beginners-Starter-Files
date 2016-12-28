f905fc7056d56227cdee097ce3a06d6eb868177e
val rigodon <rvrigodon@gmail.com>
2016-12-28 13:35:23 -0500

    This commit is for removing items from the order list

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
     this.addFish = this.addFish.bind(this);
      this.loadSamples = this.loadSamples.bind(this);
       this.addToOrder = this.addToOrder.bind(this);
+       this.removeFromOrder = this.removeFromOrder.bind(this);
       this.updateFish = this.updateFish.bind(this);
       this.removeFish = this.removeFish.bind(this);
 
@@ -106,6 +107,14 @@ this.setState({order})
 
 }
 
+
+removeFromOrder(key){
+const order = {...this.state.order};
+delete order[key];
+this.setState({ order })
+
+}
+
 render () {
 return (
 
@@ -119,7 +128,7 @@ return (
                   }
                 </ul>
             </div>
-          <Order fishes={this.state.fishes} order={this.state.order}  params={this.props.params} />
+          <Order fishes={this.state.fishes} order={this.state.order}  params={this.props.params}  removeFromOrder={this.removeFromOrder}/>
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}  fishes={this.state.fishes} updateFish={this.updateFish}
          removeFish={this.removeFish} />
         </div>
